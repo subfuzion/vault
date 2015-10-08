f213d02033b9b9203b3e8c39da73d9f16f9ba818
Tony Pujals <tonypujals@users.noreply.github.com>
2015-10-08 00:41:35 -0700

    refactored

--- test/test.js
+++ test/test.js
@@ -1,26 +1,26 @@
-var crypt = require('../main');
+var vault = require('../lib/vault');
 var assert = require('assert');
-var path = require('path');
+var fs = require('fs');
 
-var vaultFile = path.resolve(__dirname, '..', 'vault.json');
+var password = 'secret';
 
 before(function() {
   // set test password
-  process.env.VAULT_PASS = 'password';
+  process.env.VAULT_PASS = password;
 });
 
 describe('tests', function() {
 
   it('should encrypt a string', function() {
     var plainText = 'hello';
-    var cipherText = crypt.encrypt(plainText);
+    var cipherText = vault.encrypt(plainText, password);
     assert.notEqual(plainText, cipherText);
   });
 
   it('should decrypt a string', function() {
     var plainText = 'hello';
-    var cipherText = crypt.encrypt(plainText);
-    var decipherText = crypt.decrypt(cipherText);
+    var cipherText = vault.encrypt(plainText, password);
+    var decipherText = vault.decrypt(cipherText, password);
     assert.notEqual(plainText, cipherText);
     assert.equal(plainText, decipherText);
   });
@@ -29,19 +29,19 @@ describe('tests', function() {
     var plainText = 'hello';
     var key = 'greeting';
 
-    crypt.put(key, plainText, function(err) {
+    vault.put(key, plainText, function(err) {
       if (err) return done(err);
 
       // verify vault exists and stored the ciphertext
       // associated with the key
-      var vault = require(vaultFile);
-      assert(vault[key]);
+      var vault_ = loadVault();
+      assert(vault_[key]);
 
       // should be encrypted
-      assert.notEqual(vault[key], plainText);
+      assert.notEqual(vault_[key], plainText);
 
       // should be able to decrypt
-      crypt.get(key, function(err, decipherText) {
+      vault.get(key, function(err, decipherText) {
         assert.equal(decipherText, plainText);
         done();
       });
@@ -49,5 +49,30 @@ describe('tests', function() {
     });
   });
 
+  it('should store and then remove a key', function(done) {
+    var plainText = 'hello';
+    var key = 'greeting';
+
+    vault.put(key, plainText, function(err) {
+      if (err) return done(err);
+
+      // verify vault exists and stored the ciphertext
+      // associated with the key
+      var vault_ = loadVault();
+      assert(vault_[key]);
+
+      // should be removed
+      vault.remove(key, function(err) {
+        if (err) return done(err);
+        var vault_ = require(vault.pathname);
+        assert(!vault_[key]);
+        done();
+      });
+
+    });
+  });
 });
 
+function loadVault() {
+  return JSON.parse(fs.readFileSync(vault.pathname, 'utf8'));
+}
